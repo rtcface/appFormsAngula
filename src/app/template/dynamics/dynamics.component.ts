d30e191b743fccca79f696c34998aa705fcbbffd
Ramiro Tepehua <rtface@gmail.com>
2021-10-08 23:07:39 -0500

    added configuration forms dinamics

--- src/app/template/dynamics/dynamics.component.ts
+++ src/app/template/dynamics/dynamics.component.ts
@@ -1,4 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+
+interface Person {
+  name: string;
+  favorites: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  name: string;
+}
 
 @Component({
   selector: 'app-dynamics',
@@ -6,11 +19,37 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DynamicsComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit(): void {
+export class DynamicsComponent {
+
+  @ViewChild('myForm') myForm!: NgForm;
+
+  person: Person = {
+    name: 'John',
+    favorites: [
+      { id: 1, name: 'fifa 2021' },
+      { id: 2, name: 'fifa 2020' },
+      { id: 3, name: 'fifa 2019' },
+      { id: 4, name: 'fifa 2018' },
+      { id: 5, name: 'fifa 2017' }
+    ]
+  };
+  
+
+  save(): void{
+    console.log(this.myForm);
+    this.myForm.resetForm();
+  }
+
+
+  txtValidate(): boolean {
+    return this.myForm?.controls.nameTxt?.invalid 
+    && this.myForm?.controls.nameTxt?.touched
+  }
+
+  delete(index: number): void {
+    console.log(this.person.favorites.splice(index, 1)); 
   }
 
 }
