f7fcadd460f1c09bb4fc4cf8de2183a9a6e48bc8
Ramiro Tepehua <rtface@gmail.com>
2021-10-13 08:09:17 -0500

    added valid field username

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,15 +12,23 @@ export class RegisterComponent implements OnInit {
   // TODO: move other components into
 
 nameLastName: string='([a-zA-Z]+) ([a-zA-Z]+)';
+emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
 
   myForm: FormGroup = this.fb.group({
     name: [,[Validators.required,Validators.pattern(this.nameLastName)] ],
+    email: [,[Validators.required,Validators.pattern(this.emailPattern)] ],
+    username: [,[Validators.required,this.validUserName] ],
   });
 
   constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+    this.myForm.reset({
+      name:'Ramiro Tepehua',
+      email: 'test@test.org',
+      username:'joeBlack'
+    });
   }
 
 
@@ -29,4 +37,18 @@ nameLastName: string='([a-zA-Z]+) ([a-zA-Z]+)';
     && this.myForm.get(field)?.touched; 
   }
 
+  save() {
+    console.log(this.myForm.value);
+    this.myForm.markAllAsTouched();
+  }
+
+  validUserName( control : FormControl ){
+    const valor:string = control.value?.trim().toLowerCase();
+    console.log(valor);
+
+    if( valor === "rtcface")
+        return{ rtcface:true };
+
+    return null;
+  }
 }
